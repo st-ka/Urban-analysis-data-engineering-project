cd738fa0ef6e62414f87b5c923f1fc0ec556fbe2
stavros_kalimeris <stavros@kalimeris.net>
2024-01-26 08:38:54 +0200

    postgre and pgadmin4 in docker attempt

--- Dockerfiles/xe_rentals_scrape.py
+++ Dockerfiles/xe_rentals_scrape.py
@@ -6,6 +6,11 @@ import re
 from zenrows import ZenRowsClient
 from datetime import date
 import os
+import numpy as np
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+from datetime import datetime
+
 
 
 # RENTALS 
@@ -39,31 +44,25 @@ for i in range(1,last_page+1):
 #print(web_urls)
 
 
-
-
 # RENTALS PAGES
+
 # Access page source code, parse, find elements and write them in csv file 
-price_list=[]
+
 title_list=[]
 level_list=[]
 url_list=[]
 address_list=[]
 price_sqm_list=[]
-    
+construction_year_list=[]  
 
 
-for web_url in web_urls[4:12]:
+for web_url in web_urls:
     try:
         response=client.get(web_url,headers=headers)
     
         xe_webpage=response.text
     
         soup=BeautifulSoup(xe_webpage,"html.parser")
-
-       # with open (f"./soups/soup_rentals{date.today}.txt",'a') as file:
-        #    file.write(str(soup)+ f"\n")
-
-
         print('OK')
     except:
         print(f"No response for {web_url}")
@@ -72,21 +71,33 @@ for web_url in web_urls[4:12]:
     
     prices=soup.find_all(name="span",class_="property-ad-price")
     titles=soup.find_all(name="div",class_="common-property-ad-title")
-    levels=soup.find_all(name="span",class_="property-ad-level")
+    levels=soup.find_all(name="div",class_="property-ad-level-container")
     url_element=soup.find_all(name="div",class_="common-property-ad-body grid-y align-justify")
     addresses=soup.find_all(name="span",class_="common-property-ad-address")
     prices_sqm=soup.find_all(name="span",class_="property-ad-price-per-sqm")
+    details=soup.find_all(name='div',class_="common-property-ad-details grid-x")
+
+
+
     
     for price in prices:
         p=re.findall(r'\d+', price.getText())
         price_list.append(int("".join(p)))
-
+       #price_list.append(price.getText().replace("\xa0"," ")) 
     
     for title in titles:
         t=re.findall(r'\d+', title.getText())
         title_list.append(int("".join(t)))
-
-    
+        #title_list.append(title.getText().replace("\n",""))
+#    
+#    for level in levels:
+#        try:
+#            #l=re.findall(r'[^A-Za-z0-9]+',level.getText())
+#            l=re.findall(r'\d\w+',level.getText())
+#            level_list.append("".join(l))
+#        except:
+#            level_list.append('Ground Floor|Basement')
+#       
     for urlz in url_element:
         urls=urlz.find('a')
         url_list.append(urls.get('href'))
@@ -96,26 +107,46 @@ for web_url in web_urls[4:12]:
         try:
             address_list.append(address_clean[0])
         except:
-            address_list.append('NA')
-
+            address_list.append('None')
     
     for price_sqm in prices_sqm:
         psqm=re.findall(r'\d+', price_sqm.getText())
         price_sqm_list.append(int("".join(psqm)))
 
+    for text in details:
+   
+        construction_years=text.find_all(name='div',class_="grid-x property-ad-construction-year-container")
+        if construction_years:
+            for years in construction_years:
+                year=re.findall(r'\d+',years.getText())
+                construction_year_list.append(int("".join(year)))
+        else:
+            construction_year_list.append("None")
 
+#print(len(price_list))
+#print(len(title_list))
+#print(len(level_list))
+#print(len(url_list))
+#print(len(address_list))
+#print(len(price_sqm_list))
+#print(len(construction_year_list))
+#
     
     
     
 #Create dictionary with column names as keys
-dict={'Sqm':title_list,'Price':price_list,'URL':url_list, 'neighbourhood':address_list, 'price_sq/m':price_sqm_list}
+
+dict={'Sqm':title_list,'Price':price_list,'URL':url_list,'neighbourhood':address_list, 'price_sq/m':price_sqm_list, 'construction_year':construction_year_list, "date_imported":date.today()}
 df=pd.DataFrame(dict)
-#create dataframe
 
 
 #duplicateRows = df[df.duplicated(['Title', 'Price','neighbourhood'])]
 #write to csv file
 #df.to_csv(f'xe_rentals_{date.today()}',mode='a',index=False, header=False)
+#
+#time.sleep(1)
+#
+#
 
 if not os.path.isfile('/app/csv/.csv'):
    df.to_csv(f'/app/csv/xe_rentals_{date.today()}.csv',index=False)
@@ -139,16 +170,17 @@ con=engine.connect()
 
 sql = """
   create table if not exists xrysi_rentals (
-  Area int,
+  Square_meters int,
   Price int,
   URL VARCHAR(50),
   neighbourhood VARCHAR(50),
-  price per sqm int,
+  price_per_sqm int,
+  construction_year int,
   date_imported date
 );
 """
 
-sql COPY xrysi_rentals FROM f"xe_rentals_{date.today()}.csv" DELIMITER ',' CSV HEADER;
+#sql COPY xrysi_rentals FROM f"xe_rentals_{date.today()}.csv" DELIMITER ',' CSV HEADER;
 
 # execute the 'sql' query
 with engine.connect().execution_options(autocommit=True) as conn:
@@ -156,10 +188,13 @@ with engine.connect().execution_options(autocommit=True) as conn:
 
 # insert the dataframe data to 'xrysi_rentals' SQL table
 with engine.connect().execution_options(autocommit=True) as conn:
-    df.to_sql(', con=conn, if_exists='append', index= False)
+    df.to_sql('xrysi_rentals', con=conn, if_exists='append', index= False)
 
 #optional
 print(pd.read_sql_query("""
-select * from xrysi
+select * from xrysi_rentals
 """, con))
+
+
+
 
